bf4e9206e08f807177d5254332dea9604f1d8677
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2019-09-19 23:42:30 +1000

    Add additional methods to AppInfoDao

--- app/src/main/java/com/example/climatetale/Data/AppInfoDao.java
+++ app/src/main/java/com/example/climatetale/Data/AppInfoDao.java
@@ -18,9 +18,22 @@ public interface AppInfoDao {
 
     //Queries
     //Update total chapters within app as a value
-    @Query("UPDATE AppInfo SET totalChapters =:total")
-    void updateTotalChapters(int total);
+    @Query("UPDATE AppInfo SET totalChapters =:total WHERE appID =:appID")
+    void updateTotalChapters(int total, int appID);
 
+    //Update total topics within app as a value
+    @Query("UPDATE AppInfo SET totalTopics =:total WHERE appID =:appID")
+    void updateTotalTopics(int total, int appID);
 
+    //Get appId where the total chapters amounts match
+    @Query("SELECT appID FROM AppInfo WHERE totalChapters =:totalChapters")
+    int getAppId(int totalChapters);
 
+    //Get total chapters for set appID
+    @Query("SELECT totalChapters FROM AppInfo WHERE appID =:appID")
+    int getTotalChapters(int appID);
+
+    //Get total topics for set appID
+    @Query("SELECT totalTopics FROM AppInfo WHERE appID =:appID")
+    int getTotalTopics(int appID);
 }
