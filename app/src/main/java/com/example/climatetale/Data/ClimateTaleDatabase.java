455c3116b86cf1014a36de36f93f469188112a6e
jazzie-b <33510723+jazzie-b@users.noreply.github.com>
2019-10-10 16:15:37 +1100

    Input values for database

--- app/src/main/java/com/example/climatetale/Data/ClimateTaleDatabase.java
+++ app/src/main/java/com/example/climatetale/Data/ClimateTaleDatabase.java
@@ -48,17 +48,64 @@ public abstract class ClimateTaleDatabase extends RoomDatabase {
     private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
         private UserInfoDao userInfoDao;
         private AppInfoDao appInfoDao;
+        private ChapterDao chapterDao;
+        private TopicDao topicDao;
+        private QuizDao quizDao;
+        private QuestionDao questionDao;
+        private AnswerDao answerDao;
 
         private PopulateDbAsyncTask(ClimateTaleDatabase db){
             userInfoDao = db.getUserInfoDao();
             appInfoDao = db.getAppInfoDao();
+            chapterDao = db.getChapterDao();
+            topicDao = db.getTopicDao();
+            quizDao = db.getQuizDao();
+            questionDao = db.getQuestionDao();
+            answerDao = db.getAnswerDao();
         }
 
         @Override
         protected Void doInBackground(Void... voids){
             //Start up data for database
             appInfoDao.insert(new AppInfo(1, 1,1));
-            userInfoDao.insert(new UserInfo(1, "Hello", 0,0,0,01));
+            userInfoDao.insert(new UserInfo(1,
+                    "Hello", 0,0,
+                    0,01));
+            chapterDao.insert(new Chapter(1, 1,
+                    "What is Climate Change?", 1,
+                    false));
+            topicDao.insert(new Topic(101, 1,
+                    "CO2 in Atmosphere",1,false));
+            quizDao.insert(new Quiz(10101,"Topic 1: CO2 in Atmosphere",
+                    101,false));
+            questionDao.insert(new Question(101011,10101,
+                    "Sample Quiz 1 Question 1",
+                    "Sample Option 1 Question 1",
+                    "Sample Option 2 Question 1",
+                    "Sample Option 3 Question 1",
+                    "Sample Option 4 Question 1",
+                    0));
+            questionDao.insert(new Question(101012,10101,
+                    "Sample Quiz 1 Question 2",
+                    "Sample Option 1 Question 2",
+                    "Sample Option 2 Question 2",
+                    "Sample Option 3 Question 2",
+                    "Sample Option 4 Question 2",
+                    0));
+            questionDao.insert(new Question(101013,10101,
+                    "Sample Quiz 1 Question 3",
+                    "Sample Option 1 Question 3",
+                    "Sample Option 2 Question 3",
+                    "Sample Option 3 Question 3",
+                    "Sample Option 4 Question 3",
+                    0));
+            answerDao.insert(new Answer(1010111,
+                    101011,2,false));
+            answerDao.insert(new Answer(1010121,
+                    101012,1,false));
+            answerDao.insert(new Answer(1010131,
+                    101013,1,false));
+
 
             return null;
         }
